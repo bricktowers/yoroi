ce86c661d60863ceb6ac19f7ae032aab27d558fe
jorbuedo <jorbuedo@users.noreply.github.com>
2025-01-22 16:31:49 +0100

    refactor(wallet-mobile): Add scroll to modals (#3793)

--- apps/wallet-mobile/src/features/Discover/common/HWNotSupportedModal.tsx
+++ apps/wallet-mobile/src/features/Discover/common/HWNotSupportedModal.tsx
@@ -2,7 +2,6 @@ import * as React from 'react'
 
 import {Button} from '../../../components/Button/Button'
 import {useModal} from '../../../components/Modal/ModalContext'
-import {Spacer} from '../../../components/Spacer/Spacer'
 import {Text} from '../../../components/Text'
 import {useStrings} from './useStrings'
 
@@ -17,22 +16,15 @@ export const useShowHWNotSupportedModal = () => {
   const strings = useStrings()
   const showHWNotSupportedModal = React.useCallback(
     ({onConfirm, onClose}: {onConfirm: Props['onConfirm']; onClose: () => void}) => {
-      openModal(strings.continueOnLedger, <HWNotSupportedModal onConfirm={onConfirm} />, modalHeight, onClose)
+      openModal({
+        title: strings.continueOnLedger,
+        content: <Text>{strings.signDataNotSupported}</Text>,
+        footer: <Button title={strings.cancel} onPress={onConfirm} />,
+        height: modalHeight,
+        onClose,
+      })
     },
-    [openModal, strings.continueOnLedger],
+    [openModal, strings.cancel, strings.continueOnLedger, strings.signDataNotSupported],
   )
   return {showHWNotSupportedModal, closeModal}
-}
-
-const HWNotSupportedModal = ({onConfirm}: Props) => {
-  const strings = useStrings()
-  return (
-    <>
-      <Text>{strings.signDataNotSupported}</Text>
-
-      <Spacer fill />
-
-      <Button title={strings.cancel} onPress={onConfirm} />
-    </>
-  )
 }
