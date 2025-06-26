d9d86ac32af4f43230e370ff1f2e92f8d4836178
jorbuedo <jorbuedo@users.noreply.github.com>
2025-06-26 11:43:33 +0200

    fix(wallet-mobile): invalidate banners on utxo change (#3966)

--- apps/wallet-mobile/src/features/Exchange/common/useBuyCryptoBanner.tsx
+++ apps/wallet-mobile/src/features/Exchange/common/useBuyCryptoBanner.tsx
@@ -1,10 +1,10 @@
 import {time} from '@yoroi/common'
 import {useNotificationManager} from '@yoroi/notifications'
 import {Chain, Notifications} from '@yoroi/types'
-import _ from 'lodash'
-import {useQuery} from 'react-query'
+import * as React from 'react'
+import {useQuery, useQueryClient} from 'react-query'
 
-import {useBalances} from '../../../yoroi-wallets/hooks'
+import {useBalances, useWalletEvent} from '../../../yoroi-wallets/hooks'
 import {Amounts, Quantities} from '../../../yoroi-wallets/utils/utils'
 import {BannerIds, showBanner} from '../../Notifications/common/banners'
 import {useSelectedWallet} from '../../WalletManager/common/hooks/useSelectedWallet'
@@ -24,28 +24,42 @@ export const useBuyCryptoBanner = () => {
   const primaryAmount = Amounts.getAmount(balances, wallet.portfolioPrimaryTokenInfo.id)
   const hasZeroPt = Quantities.isZero(primaryAmount.quantity)
 
+  const queryKey = ['buyCryptoBanner', wallet?.id, network]
+  const queryClient = useQueryClient()
+
+  useWalletEvent(wallet, 'utxos', () => queryClient.invalidateQueries(queryKey))
+
+  React.useEffect(() => {
+    queryClient.invalidateQueries(queryKey)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [network])
+
   useQuery({
-    queryKey: ['buyCryptoBanner', wallet?.id, network],
-    staleTime: time.oneHour,
+    queryKey,
+    staleTime: time.fiveMinutes,
     queryFn: async () => {
       if (hasZeroPt) {
         const last = (await manager.events.read()).find(
-          (ev) =>
-            ev.trigger === Notifications.Trigger.Banner &&
-            (ev.id === BannerIds.BuyCrypto || ev.id === BannerIds.TestAda),
+          (ev) => ev.trigger === Notifications.Trigger.Banner && ev.id === BannerIds.BuyCrypto,
         )
 
-        if (!last || new Date(last.date).getTime() + time.oneMonth < Date.now()) {
-          if (network === Chain.Network.Preprod) {
-            manager.events.remove(BannerIds.BuyCrypto)
+        const lastPreprod = (await manager.events.read()).find(
+          (ev) => ev.trigger === Notifications.Trigger.Banner && ev.id === BannerIds.TestAda,
+        )
+
+        if (network === Chain.Network.Preprod) {
+          manager.events.remove(BannerIds.BuyCrypto)
+          if (!lastPreprod || new Date(lastPreprod.date).getTime() + time.oneMonth < Date.now()) {
             showBanner({
               id: BannerIds.TestAda,
               title: strings.preprodFaucetBannerTitle,
               body: strings.preprodFaucetBannerText,
-              isRead: !!last,
+              isRead: !!lastPreprod,
             })
-          } else {
-            manager.events.remove(BannerIds.TestAda)
+          }
+        } else {
+          manager.events.remove(BannerIds.TestAda)
+          if (!last || new Date(last.date).getTime() + time.oneMonth < Date.now()) {
             showBanner({
               id: BannerIds.BuyCrypto,
               title: strings.needMoreCrypto,
